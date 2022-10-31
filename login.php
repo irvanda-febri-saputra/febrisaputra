851e75d8d3bc14d7119f4550c177e3374f813dba
irvanda-febri-saputra <irvanda150202@gmail.com>
2022-10-31 19:58:26 +0700

    project1

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <form action="" method="post" class="login-email">
+            <p style="font-size: 2rem; font-weight:850;">Login</p>
+            <div class="input-group">
+                <input type="text" placeholder="Username" name="username">
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="password" name="password">
+            </div>
+            <div class="input-group">
+                <button name="submit" class="btn">Register</button>
+            </div>
+            <p class="login-register-text">Belum punya akun?</p>
+            <a href="register.php">Register</a>
+            </div>
+            
+        </form>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <form action="" method="post" class="login-email">
+            <p style="font-size: 2rem; font-weight:850;">Register</p>
+            <div class="input-group">
+                <input type="text" placeholder="Username" name="username">
+            </div>
+
+            <div class="input-group">
+                <input type="text" placeholder="Email" name="email">
+            </div>
+
+            <div class="input-group">
+                <input type="password" placeholder="password" name="password">
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder=" Confirm password" name="cppassword">
+            </div>
+            <div class="input-group">
+                <button name="submit" class="btn">Register</button>
+            </div>
+            <p class="login-register-text">Sudah punya akun?</p>
+            <a href="login.php">Login</a>
+            </div>
+            
+        </form>
+    </div>
+</body>
+</html>
